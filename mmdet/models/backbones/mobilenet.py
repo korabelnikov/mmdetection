a07614e623efed11e6007ab5f21574c8f89b96ae
korabelnikov <nesoriti@yandex.ru>
2019-10-29 18:49:19 +0300

    add mobilenet as backbone

--- mmdet/models/backbones/mobilenet.py
+++ mmdet/models/backbones/mobilenet.py
@@ -1,93 +1,111 @@
+from typing import Tuple
+
 from ..registry import BACKBONES
+# borrowed from "https://github.com/marvis/pytorch-mobilenet"
+
 import torch.nn as nn
-from torchvision.models.mobilenet import InvertedResidual, _make_divisible, ConvBNReLU
+import torch.nn.functional as F
 
 
-class MobileNetV2(nn.Module):
-    def __init__(self, num_classes=1000, width_mult=1.0, inverted_residual_setting=None, round_nearest=8,
-                 last_channel=1280):
-        """
-        MobileNet V2 main class
+class MobileNetV1(nn.Module):
+    def __init__(self, num_classes=1024):
+        super(MobileNetV1, self).__init__()
 
-        TODO: there is a copy from torch vision. It was impossible to change `last_channel` cause
-        it is hard-coded as constant. Changing and rebuilding of vision is also not a good idea for now.
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
 
-        Args:
-            num_classes (int): Number of classes
-            width_mult (float): Width multiplier - adjusts number of channels in each layer by this amount
-            inverted_residual_setting: Network structure
-            round_nearest (int): Round the number of channels in each layer to be a multiple of this number
-            Set to 1 to turn off rounding
-        """
-        super(MobileNetV2, self).__init__()
-        block = InvertedResidual
-        input_channel = 32
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
 
-        if inverted_residual_setting is None:
-            inverted_residual_setting = [
-                # t, c, n, s
-                [1, 16, 1, 1],
-                [6, 24, 2, 2],
-                [6, 32, 3, 2],
-                [6, 64, 4, 2],
-                [6, 96, 3, 1],
-                [6, 160, 3, 2],
-                [6, 320, 1, 1],
-            ]
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
 
-        # only check the first element, assuming user knows t,c,n,s are required
-        if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
-            raise ValueError("inverted_residual_setting should be non-empty "
-                             "or a 4-element list, got {}".format(inverted_residual_setting))
-
-        # building first layer
-        input_channel = _make_divisible(input_channel * width_mult, round_nearest)
-        self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
-        #features = [ConvBNReLU(3, input_channel, stride=2)]
-        features = [ConvBNReLU(3, input_channel, stride=1)]
-        # building inverted residual blocks
-        for t, c, n, s in inverted_residual_setting:
-            output_channel = _make_divisible(c * width_mult, round_nearest)
-            for i in range(n):
-                stride = s if i == 0 else 1
-                features.append(block(input_channel, output_channel, stride, expand_ratio=t))
-                input_channel = output_channel
-        # building last several layers
-        features.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1))
-        # make it nn.Sequential
-        self.features = nn.Sequential(*features)
-
-        # building classifier
-        self.classifier = nn.Sequential(
-            nn.Dropout(0.2),
-            nn.Linear(self.last_channel, num_classes),
+        self.model = nn.Sequential(
+            conv_bn(3, 32, 2),
+            conv_dw(32, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 256, 1),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 512, 1),
+            conv_dw(512, 1024, 2),
+            conv_dw(1024, 1024, 1),
         )
-
-        # weight initialization
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.zeros_(m.bias)
+        #self.fc = nn.Linear(1024, num_classes)
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.mean([2, 3])
-        x = self.classifier(x)
+        x = self.model(x)
+        x = F.avg_pool2d(x, 7)
+        x = x.view(-1, 1024)
+        #x = self.fc(x)
         return x
 
 
+from torch.nn import Conv2d, Sequential, ModuleList, ReLU
+
 @BACKBONES.register_module
-class MobileNet(MobileNetV2):
+class MobileNet(MobileNetV1):
+    def __init__(self, out_feature_indices=(11, 13), **kwargs):
+        super().__init__(**kwargs)
+        self.extras = ModuleList([
+            Sequential(
+                Conv2d(in_channels=1024, out_channels=256, kernel_size=1),
+                ReLU(),
+                Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1),
+                ReLU()
+            ),
+            Sequential(
+                Conv2d(in_channels=512, out_channels=128, kernel_size=1),
+                ReLU(),
+                Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1),
+                ReLU()
+            ),
+            Sequential(
+                Conv2d(in_channels=256, out_channels=128, kernel_size=1),
+                ReLU(),
+                Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1),
+                ReLU()
+            ),
+            Sequential(
+                Conv2d(in_channels=256, out_channels=128, kernel_size=1),
+                ReLU(),
+                Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1),
+                ReLU()
+            ),
+            Sequential(
+                Conv2d(in_channels=256, out_channels=128, kernel_size=1),
+                ReLU(),
+                Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1),
+                ReLU()
+            )
+        ])
+        self.out_feature_indices = out_feature_indices
+
     def forward(self, x):
-        x = self.features(x)
-        return x,  # should return a tuple
+        outs = []
+        for i, layer in enumerate(self.model):
+            x = layer(x)
+            if i in self.out_feature_indices:
+                outs.append(x)
+        for layer in self.extras:
+            x = layer(x)
+            outs.append(x)
+
+        return tuple(outs)  # should return a tuple
 
     def init_weights(self, pretrained=None):
         if pretrained:
